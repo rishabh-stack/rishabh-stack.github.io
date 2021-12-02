d3edf985e80f4224064fe47d833abe39eacd8b68
rishabh-stack <rishabhsharma952000@gmail.com>
2021-12-02 20:59:25 +0530

    [UPDATE] resume

--- index.js
+++ index.js
@@ -60,3 +60,141 @@ function complete() {
     clearInterval(timer);
     timer = null;
 }
+
+// skills
+var skills = [
+    {"header" : "INTERESTS",
+      "captions" : [
+        "Software Development",
+        "CI/CD",
+        "Web",
+        "Machine Learning",
+        "AI"
+      ],
+      "values" : [
+        0.80,
+        0.90,
+        0.70,
+        0.80,
+        0.90
+      ]
+    },
+    {"header" : "LANGUAGES",
+      "captions" : [
+        "Python",
+        "PhP",
+        "Java",
+        "Javascript",
+        "c++"
+      ],
+      "values" : [
+        0.90,
+        0.70,
+        0.90,
+        0.70,
+        0.80
+      ]
+    },
+    {"header" : "FRAMEWORKS AND DATABASES",
+      "captions" : [
+        "Django",
+        "Nodejs",
+        "React",
+        "MongoDb",
+        "Mysql"
+      ],
+      "values" : [
+        0.80,
+        0.85,
+        0.90,
+        0.75,
+        0.60
+      ]
+    }
+  ];
+
+  var pentagonIndex = 0;
+  var valueIndex = 0;
+  var width = 0;
+  var height = 0;
+  var radOffset = Math.PI/2
+  var sides = 5; // Number of sides in the polygon
+  var theta = 2 * Math.PI/sides; // radians per section
+
+  function getXY(i, radius) {
+    return {"x": Math.cos(radOffset +theta * i) * radius*width + width/2,
+      "y": Math.sin(radOffset +theta * i) * radius*height + height/2};
+  }
+
+  var hue = [];
+  var hueOffset = 35;
+
+  for (var s in skills) {
+    $(".skills").append('<div class="pentagon col-lg-4 col-md-4 col-sm-6 col-12" id="interests"><div class="header"></div><canvas width="294" height="269" class="pentCanvas" /></div>');
+    hue[s] = (hueOffset + s * 255/skills.length) % 255;
+  }
+
+  $(".pentagon").each(function(index){
+    width = $(this).width();
+    height = $(this).height();
+    var ctx = $(this).find('canvas')[0].getContext('2d');
+    ctx.canvas.width = width;
+    ctx.canvas.height = height;
+    ctx.font="15px Monospace";
+    ctx.textAlign="center";
+
+    /*** LABEL ***/
+    color = "hsl("+hue[pentagonIndex]+", 100%, 30%)";
+    ctx.fillStyle = color;
+    ctx.fillText(skills[pentagonIndex].header, width/2, 15);
+    ctx.font="13px Monospace";
+
+    /*** PENTAGON BACKGROUND ***/
+    for (var i = 0; i < sides; i++) {
+      // For each side, draw two segments: the side, and the radius
+      ctx.beginPath();
+      xy = getXY(i, 0.3);
+      colorJitter = 25 + theta*i*2;
+      color = "hsl("+hue[pentagonIndex]+",100%," + colorJitter + "%)";
+      ctx.fillStyle = color;
+      ctx.strokeStyle = color;
+      ctx.moveTo(0.5*width, 0.5*height); //center
+      ctx.lineTo(xy.x, xy.y);
+      xy = getXY(i+1, 0.3);
+      ctx.lineTo(xy.x, xy.y);
+      xy = getXY(i, 0.37);
+      console.log();
+      ctx.fillText(skills[ pentagonIndex].captions[valueIndex],xy.x, xy.y +5);
+      valueIndex++;
+      ctx.closePath();
+      ctx.fill();
+      ctx.stroke();
+    }
+
+    valueIndex = 0;
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+    ctx.strokeStyle = "rgba(0, 0, 0, 0.3)";
+    ctx.lineWidth = 5;
+    var value = skills[pentagonIndex].values[valueIndex];
+    xy = getXY(i, value * 0.3);
+    ctx.moveTo(xy.x,xy.y);
+    /*** SKILL GRAPH ***/
+    for (var i = 0; i < sides; i++) {
+      xy = getXY(i, value * 0.3);
+      ctx.lineTo(xy.x,xy.y);
+      valueIndex++;
+      value = skills[pentagonIndex].values[valueIndex];
+    }
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+    valueIndex = 0;
+    pentagonIndex++;
+  });
+  $(".nav .nav-link").on("click", function(){
+    $(".nav").find(".active").removeClass("active");
+    $(this).addClass("active");
+ });
+
+  
